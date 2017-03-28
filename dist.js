66af200c2eb8fd980b8ca604873c5be7db9fcf7a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-03-28 16:02:38 +0200

    code cleanup / progress\

--- dist.js
+++ dist.js
@@ -839,15 +839,8 @@ module.exports = sea;
 
 var cons = {};
 var connecting = {};
-function str(o) {
-  try {
-    return JSON.stringify(o);
-  } catch(e) {
-    o.toString();
-  }
-}
-
-function log() {
+// ## Utility functions
+function log() { // ###
   let s = str(slice(arguments))
   //console.log(s);
   console.log.apply(console, arguments);
@@ -864,24 +857,14 @@ function log() {
 }
 
 main();
-var iceServers; // ##
-// Stun server list from https://gist.github.com/zziuni/3741933 
-iceServers = ['stun.l.google.com:19302', 'stun1.l.google.com:19302',
-           'stun2.l.google.com:19302', 'stun3.l.google.com:19302',
-           'stun4.l.google.com:19302', 'stun01.sipphone.com', 'stun.ekiga.net',
-           'stun.fwdnet.net', 'stun.ideasip.com', 'stun.iptel.org',
-           'stun.rixtelecom.se', 'stun.schlund.de', 'stunserver.org',
-           'stun.softjoys.com', 'stun.voiparound.com', 'stun.voipbuster.com',
-           'stun.voipstunt.com', 'stun.voxgratia.org', 'stun.xten.com'];
-iceServers = iceServers.map(s => ({url: 'stun:' + s}));
-function hashDist(s1, s2) { // ##
+function hashDist(s1, s2) { // ###
   return HashAddress.fromHex(s1).dist(HashAddress.fromHex(s2));
 }
 
 function slice(o, a, b) { return Array.prototype.slice.call(o, a, b); }
 let randomId = () => Math.random().toString(36).slice(2,10);
 let timeout = 5000;
-let findMin = (arr, f) => { // ##
+function findMin(arr, f) { // ###
   arr = arr || [];
   let min = arr[0];
   for(let i = 1; i < arr.length; ++i) {
@@ -892,9 +875,170 @@ let findMin = (arr, f) => { // ##
   return min;
 }
 
-exportFn('call', call); // ##
+function encode(obj) { // ###
+  //return bion.encode(obj).buffer;
+  return JSON.stringify(obj);
+}
 
-function call(dst, type) { // ##
+function decode(obj) { // ###
+  //return bion.decode(new Uint8Array(obj));
+  return JSON.parse(obj);
+}
+
+function sleep(ms) {// ###
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function str(o) { // ###
+  try {
+    return JSON.stringify(o);
+  } catch(e) {
+    o.toString();
+  }
+}
+
+async function generateId() { // ###
+  let key = await crypto.subtle.generateKey({
+    name: 'ECDSA', 
+    namedCurve: 'P-521'
+  }, true, ['sign', 'verify']);
+  publicKey = await crypto.subtle.exportKey('spki', key.publicKey);
+  return (await HashAddress.generate(publicKey)).toHex();
+}
+
+// ## WebSocket connections
+function startWsServer() { // ###
+  let server = node_modules.http.createServer();
+  let  wss = new node_modules.ws.Server({server: server});
+
+  let sockets = {};
+  wss.on('connection', (ws) => {
+    let con = new EventEmitter();
+    con.send = (o) => ws.send(encode(o));
+    con.close = (o) => ws.close();
+
+    sea.emit('connect:socket', con);
+    ws.on('message', (msg) => con.emit('message', decode(msg)));
+    ws.on('close', () => con.emit('close'));
+    ws.on('error', (e) => con.emit('error', e));
+  });
+
+  server.listen(8888, () => {
+    log('Started websocket server on port: ' + server.address().port);
+  });
+}
+
+function connectToWs(host) { // ###
+  return new Promise((resolve, reject) => {
+    let ws = new WebSocket(host);
+    ws.binaryType = 'arraybuffer';
+    let con = new EventEmitter();
+    con.send = (o) => ws.send(encode(o));
+    con.close = () => ws.close();
+    ws.addEventListener('message', (msg) => {
+      con.emit('message', decode(msg.data));
+    });
+    ws.addEventListener('open', (msg) => {
+      sea.emit('connect:socket', con, resolve);
+    });
+    ws.addEventListener('close', (msg) => {
+      con.emit('close');
+    });
+    ws.addEventListener('error', (msg) => {
+      reject(msg);
+    });
+  });
+}
+
+// ## WebRTC Connections
+var iceServers; // ###
+// Stun server list from https://gist.github.com/zziuni/3741933 
+iceServers = ['stun.l.google.com:19302', 'stun1.l.google.com:19302',
+  'stun2.l.google.com:19302', 'stun3.l.google.com:19302',
+  'stun4.l.google.com:19302', 'stun01.sipphone.com', 'stun.ekiga.net',
+  'stun.fwdnet.net', 'stun.ideasip.com', 'stun.iptel.org',
+  'stun.rixtelecom.se', 'stun.schlund.de', 'stunserver.org',
+  'stun.softjoys.com', 'stun.voiparound.com', 'stun.voipbuster.com',
+  'stun.voipstunt.com', 'stun.voxgratia.org', 'stun.xten.com'];
+iceServers = iceServers.map(s => ({url: 'stun:' + s}));
+
+
+let sockets = {}
+async function connectVia(through, id) { // ###
+
+  let con = new RTCPeerConnection({ 'iceServers': iceServers });
+  con.onicecandidate = iceHandler(through, id);
+  con.onerror = log;
+
+  if(!sockets[id]) { sockets[id] = { connected: false, con }; }
+
+  let chan = con.createDataChannel("sendChannel", { ordered: false });
+  addChanHandler(chan, id);
+
+  let offer = await con.createOffer();
+  await con.setLocalDescription(offer);
+
+  let answer = await call(through, 'call', 
+    id, 'webrtc-offer', through, sea.id, con.localDescription);
+  log('got answer:', answer);
+  con.setRemoteDescription(answer);
+  sockets[id].connected = true;
+  connecting[id] = {id, con, offer, chan}
+}
+
+exportFn('webrtc-offer', async (through, id, offer) => { // ###
+  log('webrtc-offer', id, offer);
+  con = new RTCPeerConnection();
+
+  if(!sockets[id]) {
+    sockets[id] = { connected: false, con };
+  }
+
+  con.ondatachannel = (e) => {
+    addChanHandler(e.channel);
+  };
+
+  con.onicecandidate = iceHandler(through, id);
+  con.onerror = log;
+
+  await con.setRemoteDescription(offer);
+  let answer = await con.createAnswer();
+  con.setLocalDescription(answer);
+  return answer;
+});
+
+
+exportFn('ice', (id, ice) => { // ###
+  log('addIce', id.slice(0, 5), cons[id])
+  if(!sockets[id].connected) {
+    sockets[id].con.addIceCandidate(ice);
+  }
+  log('ice', id, ice);
+});
+
+function addChanHandler(chan, id) { // ###
+  chan.onmessage = (e) => log('msg', e.data);
+  chan.onopen = () => {
+    this.sockets[id].connected = true;
+    this.sockets[id].chan = chan;
+    chan.send('hello');
+  }
+  if(chan.readyState === "open") {
+    chan.onopen();
+  }
+}
+
+function iceHandler(through, id) { // ###
+  return (e) => {
+    if(!sockets[id].connected) {
+      if(e.candidate) {
+        call(through, 'call', id, 'ice', sea.id, e.candidate);
+      }
+    }
+  }
+}
+// ## General communication structure
+function call(dst, type) { // ###
   let args = slice(arguments, 2);
   log('call ' + type, dst, type, args, arguments);
   return new Promise((resolve, reject) => {
@@ -905,7 +1049,7 @@ function call(dst, type) { // ##
   });
 }
 
-function exportFn(name, f) { // ##
+function exportFn(name, f) { // ###
   sea.net.on(name, async (msg) => {
     let response = { type: msg.srcType, dst: msg.src };
     try {
@@ -917,7 +1061,8 @@ function exportFn(name, f) { // ##
   });
 }
 
-function relay(msg) { // ##
+exportFn('call', call); // ###
+function relay(msg) { // ###
   log('relay ' + msg.dst.slice(0,8) + ' ' + msg.type);
   let con = findMin(Object.values(cons), o => hashDist(o.id, msg.dst));
   if(hashDist(con.id, msg.dst) < hashDist(sea.id, msg.dst)) {
@@ -926,7 +1071,7 @@ function relay(msg) { // ##
     log('dropped', msg.type, msg.dst);
   }
 }
-sea.on('connect:socket', (con, resolve) => { // ##
+sea.on('connect:socket', (con, resolve) => { // ###
   con.send({
     type: 'helo',
     id: sea.id,
@@ -959,7 +1104,10 @@ sea.on('connect:socket', (con, resolve) => { // ##
   });
 });
 
-async function main() { // ##
+// ## Main
+async function main() { // ###
+  sea.id = await generateId();
+  log('My id: ' + sea.id);
   await generateId();
   if(self.node_modules) {
     startWsServer();
@@ -976,131 +1124,22 @@ async function main() { // ##
     }
   }
 }
-async function connectVia(a, id) { // ##
-  let con = new RTCPeerConnection({ 'iceServers': iceServers });
-  con.onicecandidate = (e) => {
-    log('onicecandidata', e);
-    if(e.candidate) {
-    }
-  }
-  let chan = con.createDataChannel("sendChannel", { ordered: false });
-  let offer = await con.createOffer();
-  await con.setLocalDescription(offer);
-  con.ondatachannel = (e) => {
-    log('ondatachannel', e);
-    e.channel.send('hi');
-  };
-  con.onerror = (e) => {
-    log('err1', e);
-  }
-  let answer = await call(a, 'call', id, 'webrtc-offer', sea.id, con.localDescription);
-  log('got answer:', answer);
-  con.setRemoteDescription(answer);
-  connecting[id] = {id, con, offer, chan}
-}
-exportFn('webrtc-offer', async (id, offer) => { // ##
-  log('webrtc-offer', id, offer);
-  con = new RTCPeerConnection();
-  con.ondatachannel = (o) => {
-    log('ondatachannel2', o);
-  };
-  con.onicecandidate = (o) => {
-    log('onicecandidate2', o);
-  };
-  con.onerror = (e) => {
-    log('err2', e);
-  }
-  await con.setRemoteDescription(offer);
-  let answer = await con.createAnswer();
-  con.setLocalDescription(answer);
-  log('answer', answer);
-  connecting[id] = { id, con, offer };
-  return answer;
-});
-
-async function goOnline() { // ##
+async function goOnline() { // ###
   await connectToWs('ws://localhost:8888/');
   let done = false;
   //do {
   let a = findMin(Object.keys(cons), o => hashDist(sea.id, o));
-  log('a', a);
+  //log('a', a);
   let b = findMin(cons[a].peers,  o => hashDist(sea.id, o));
-  log('b', b);
+  //log('b', b);
   if(!b || hashDist(sea.id, a) < hashDist(sea.id, b)) {
+    log('connected');
     done = true;
   } else {
+    log('connectVia', a.slice(0,5), b.slice(0,5));
     await connectVia(a, b);
   }
   //  } while(!done);
-}
-
-async function generateId() { // ##
-  let key = await crypto.subtle.generateKey({
-    name: 'ECDSA', 
-    namedCurve: 'P-521'
-  }, true, ['sign', 'verify']);
-  publicKey = await crypto.subtle.exportKey('spki', key.publicKey);
-  sea.id = (await HashAddress.generate(publicKey)).toHex();
-  sea.emit('ready');
-  log('My id: ' + sea.id);
-}
-
-function connectToWs(host) { // ##
-  return new Promise((resolve, reject) => {
-    let ws = new WebSocket(host);
-    ws.binaryType = 'arraybuffer';
-    let con = new EventEmitter();
-    con.send = (o) => ws.send(encode(o));
-    con.close = () => ws.close();
-    ws.addEventListener('message', (msg) => {
-      con.emit('message', decode(msg.data));
-    });
-    ws.addEventListener('open', (msg) => {
-      sea.emit('connect:socket', con, resolve);
-    });
-    ws.addEventListener('close', (msg) => {
-      con.emit('close');
-    });
-    ws.addEventListener('error', (msg) => {
-      reject(msg);
-    });
-  });
-}
-
-function startWsServer() { // ##
-  let server = node_modules.http.createServer();
-  let  wss = new node_modules.ws.Server({server: server});
-
-  let sockets = {};
-  wss.on('connection', (ws) => {
-    let con = new EventEmitter();
-    con.send = (o) => ws.send(encode(o));
-    con.close = (o) => ws.close();
-
-    sea.emit('connect:socket', con);
-    ws.on('message', (msg) => con.emit('message', decode(msg)));
-    ws.on('close', () => con.emit('close'));
-    ws.on('error', (e) => con.emit('error', e));
-  });
-
-  server.listen(8888, () => {
-    log('Started websocket server on port: ' + server.address().port);
-  });
-}
-
-// ## Utility functions
-function encode(obj) { // ###
-  //return bion.encode(obj).buffer;
-  return JSON.stringify(obj);
-}
-
-function decode(obj) { // ###
-  //return bion.decode(new Uint8Array(obj));
-  return JSON.parse(obj);
-}
-
-function sleep(ms) {// ###
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // # Old
@@ -1235,7 +1274,7 @@ function sleep(ms) {// ###
     "Links":[],
   };
   var cbor = CBOR.encode(json)
-    var ua = Array.from(new Uint8Array(cbor));
+  var ua = Array.from(new Uint8Array(cbor));
   log(cbor.byteLength, String.fromCharCode.apply(null, ua), ua.map(o => o.toString(16)));
   var d1 = new Uint8Array(cbor.byteLength + 1);
   log(d1, cbor);
